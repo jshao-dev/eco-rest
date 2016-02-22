da79921b816cd81f6c5edf9138d1680747349ce7
Jun Shao <jshao@mdsol.com>
2016-02-22 15:14:48 -0500

    Windows install both PowerShell and batch commands

--- public/install.ps1
+++
@@ -1,9 +0,0 @@
-$moduleHome = "$env:USERPROFILE\Documents\WindowsPowerShell\Modules\eco\eco.psm1"
-new-item $moduleHome -ItemType file -Force | out-null
-@"
-function eco(){
-    `$url = "http://10.67.29.129:3000/use/`$(`$args -join "/")/?format=ps1"
-    iex ((new-object net.webclient).DownloadString(`$url))
-}
-Export-ModuleMember -Function eco
-"@ | out-file $moduleHome -Force utf8
